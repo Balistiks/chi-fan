887c3da1c8180f13d11c4d19276bf240dc248dcd
balistik <balistiktw@gmail.com>
2024-12-25 01:14:42 +1000

    make click work dates

--- webapp/src/pages/index-page/ui/index.jsx
+++ webapp/src/pages/index-page/ui/index.jsx
@@ -4,6 +4,13 @@ import styles from './styles.module.scss'
 import ShiftDayModal from "../../../widgets/shift-day-modal/ui";
 import {useState} from "react";
 
+const dates = [
+  new Date(2024, 11, 1),
+  new Date(2024, 11, 15),
+  new Date(2024, 11, 30),
+  new Date(2024, 11, 10),
+]
+
 const IndexPage = () => {
   const [date, setDate] = useState(new Date());
   const [modalOpen, setModalOpen] = useState(false)
@@ -19,9 +26,13 @@ const IndexPage = () => {
           nextLabel={<img src={'/icons/arrow right.svg'} alt={'arrow right'}/>}
           prevLabel={<img src={'/icons/arrow left.svg'} alt={'arrow left'}/>}
           onClickDay={(value) => {
-            setDate(value)
-            setModalOpen(true)
+            if (!!dates.find((item) => item.getTime() === value.getTime())) {
+              setDate(value)
+              setModalOpen(true)
+            }
           }}
+          tileClassName={({ date, view }) =>
+            !!dates.find((item) => item.getTime() === date.getTime()) ? styles.day : null}
         />
         { modalOpen && <ShiftDayModal setModalOpen={setModalOpen} /> }
       </div>
