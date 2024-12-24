40348900270bc74c74fcabcdabfd5ce8a20afa93
balistik <balistiktw@gmail.com>
2024-12-24 10:54:03 +1000

    make calendar

--- webapp/src/pages/index-page/ui/index.jsx
+++ webapp/src/pages/index-page/ui/index.jsx
@@ -1,9 +1,19 @@
-import {Calendar} from "../../../widgets/calendar";
+import Calendar from 'react-calendar';
+import './calendar.css'
+import styles from './styles.module.scss'
 
 const IndexPage = () => {
   return (
     <>
-      <Calendar />
+      <div className={styles.calendarHandler}>
+        <Calendar
+          className={styles.calendar}
+          onClickMonth={null}
+          minDetail={'month'}
+          nextLabel={<img src={'/icons/arrow right.svg'} alt={'arrow right'}/>}
+          prevLabel={<img src={'/icons/arrow left.svg'} alt={'arrow left'}/>}
+        />
+      </div>
     </>
   )
 }
