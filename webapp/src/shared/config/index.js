1d4888bff8e3244dd60d4e50c6cb2c9a8c359240
balistik <balistiktw@gmail.com>
2025-01-26 04:31:23 +1000

    mb fix

--- webapp/src/shared/config/index.js
+++ webapp/src/shared/config/index.js
@@ -8,4 +8,4 @@ export const WEB_URL = process.env.NODE_ENV === 'development'
 
 export const API_URL_PATH = (path) => API_URL + path;
 
-export const token = import.meta.env.VITE_SECRET_TOKEN
+export const token = import.meta.env.SECRET_TOKEN
