c404b10db86a94ef72b8294ae3727915d138f039
Иван Афанасьев <afanasevv@120-pool.net.vvsu.ru>
2025-01-26 01:18:43 +1000

    fix

--- back/src/revenues/revenues.controller.ts
+++ back/src/revenues/revenues.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param } from "@nestjs/common";
 import { RevenuesService } from './revenues.service';
 import { Revenue } from './entities/revenue.entity';
+import { Between } from "typeorm";
 
 @Controller('revenues')
 export class RevenuesController {
@@ -8,19 +9,37 @@ export class RevenuesController {
 
   @Get('all')
   async getAllPoints(): Promise<Revenue[]> {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth();
+    const firstDayOfMonth = new Date(currentYear, currentMonth, 1);
+    const lastDayOfMonth = new Date(currentYear, currentMonth + 1, 0);
+
+
     return await this.revenuesService.getAll({
       relations: ['point'],
+      where: {
+        date: Between(firstDayOfMonth, lastDayOfMonth)
+      }
     });
   }
 
+
   @Get(':pointId')
   async getByDays(@Param('pointId') pointId: number): Promise<Revenue[]> {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth();
+    const firstDayOfMonth = new Date(currentYear, currentMonth, 1);
+    const lastDayOfMonth = new Date(currentYear, currentMonth + 1, 0);
+
     return await this.revenuesService.getAll({
       relations: ['point'],
       where: {
         point: {
           id: pointId,
         },
+        date: Between(firstDayOfMonth, lastDayOfMonth)
       },
       order: {
         date: 'ASC',
@@ -28,13 +47,21 @@ export class RevenuesController {
     });
   }
 
+
   @Get('amount/:id')
   async getOne(@Param('id') id: number): Promise<Revenue> {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth(); 
+    const firstDayOfMonth = new Date(currentYear, currentMonth, 1);
+    const lastDayOfMonth = new Date(currentYear, currentMonth + 1, 0);
     return await this.revenuesService.findOne({
       where: {
         id: id,
+        date: Between(firstDayOfMonth, lastDayOfMonth)
       },
       relations: ['point'],
     });
   }
+
 }
