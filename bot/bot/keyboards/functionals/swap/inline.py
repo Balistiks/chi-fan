94ed61d7442678f5f4bfaf202d7c4de2b288b024
balistik <balistiktw@gmail.com>
2025-01-26 05:18:42 +1000

    add webapp to keyboard

--- bot/bot/keyboards/functionals/swap/inline.py
+++ bot/bot/keyboards/functionals/swap/inline.py
@@ -1,9 +1,9 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
 SWAP_NO_KEYBOARD = InlineKeyboardMarkup(
     inline_keyboard=[
         [
-            InlineKeyboardButton(text='Мои смены', callback_data='#'),
+            InlineKeyboardButton(text='Мои смены', web_app=WebAppInfo(url='https://chifan-corp.ru')),
         ],
         [
             InlineKeyboardButton(text='Вернуть в главное меню', callback_data='main_menu'),
