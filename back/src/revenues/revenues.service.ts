668fbc332e681ae714d720c4f2e132c5ba0a9a49
balistik <balistiktw@gmail.com>
2025-01-29 18:07:22 +1000

    fix parse

--- back/src/revenues/revenues.service.ts
+++ back/src/revenues/revenues.service.ts
@@ -29,28 +29,30 @@ export class RevenuesService implements OnApplicationBootstrap {
         const dateArray = line[0].split('.');
         const date = new Date(dateArray[2], +dateArray[1] - 1, dateArray[0]);
         for (let i = 1; i < data[0].length - 2; i++) {
-          if (line[i] != '' && (line[i] != null || line[i] != undefined)) {
-            const point = await this.pointsService.findOne({
-              where: {
-                name: data[0][i],
-              },
-            });
-            const revenue = await this.revenueRepository.findOne({
-              where: {
-                point,
-                date,
-              },
-            });
-            const amount = line[i].replace(/\s+/g, '').replace('₽', '');
-            if (revenue != null) {
-              revenue.amount = amount;
-              await this.revenueRepository.save(revenue);
-            } else {
-              const newRevenue = new Revenue();
-              newRevenue.point = point;
-              newRevenue.date = date;
-              newRevenue.amount = amount;
-              await this.revenueRepository.save(newRevenue);
+          if (data[0][i] != 'итого') {
+            if (line[i] != '' && (line[i] != null || line[i] != undefined)) {
+              const point = await this.pointsService.findOne({
+                where: {
+                  name: data[0][i],
+                },
+              });
+              const revenue = await this.revenueRepository.findOne({
+                where: {
+                  point,
+                  date,
+                },
+              });
+              const amount = line[i].replace(/\s+/g, '').replace('₽', '');
+              if (revenue != null) {
+                revenue.amount = amount;
+                await this.revenueRepository.save(revenue);
+              } else {
+                const newRevenue = new Revenue();
+                newRevenue.point = point;
+                newRevenue.date = date;
+                newRevenue.amount = amount;
+                await this.revenueRepository.save(newRevenue);
+              }
             }
           }
         }
